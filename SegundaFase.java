2fb86ccbaa5ad1f4c367b55a28dfcecbd7049516
GabrielLomba12 <lombagabriel324@gmail.com>
2023-04-20 22:52:59 -0300

    alterção na regiao

--- SegundaFase.java
+++ SegundaFase.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class Fase2 {
+public class SegundaFase {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
